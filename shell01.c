347787a10a21a7c801635560db51beeec161c64e
RonaldPondo <ronaldpondo@outlook.com>
2023-05-24 08:59:23 -0700

    task1

---
+++ shell01.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+int main() {
+  char *prompt = "simple_shell> ";
+  char *command;
+
+  while (1) {
+    printf("%s", prompt);
+    fflush(stdout);
+
+    command = getline(NULL, 0, stdin);
+    if (command == NULL) {
+      break;
+    }
+
+    if (command[0] == '\n' || command[0] == '\0') {
+      continue;
+    }
+
+    pid_t pid = fork();
+    if (pid == 0) {
+      if (execvp(command, &command) < 0) {
+        perror("execvp");
+        exit(1);
+      }
+    } else if (pid < 0) {
+      perror("fork");
+      exit(1);
+    }
+
+    waitpid(pid, NULL, 0);
+  }
+
+  return 0;
+}
